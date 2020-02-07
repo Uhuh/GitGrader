011575d71e8c45ed047c4a10966e1ca6e8469f9a
Dylan Warren <duwtgb@mst.edu>
2020-02-06 19:24:16 -0600

    Some known needed shit

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const App = () => (
-  <div>What's up fools</div>
+  <div>What's up dddfools</div>
 );
 
 ReactDOM.render(<App/>, document.getElementById("app"));
