87cccb964bac8526126011fa94376a36927ec55b
Dylan Warren <duwtgb@mst.edu>
2020-02-10 20:59:36 -0600

    Idk something

---
+++ src/modules/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { HomeNav } from './navs';
+
+export const App = () => (
+  <>
+    <HomeNav />
+    <div>
+      <p>Some random stufffff</p>
+    </div>
+  </>
+);
