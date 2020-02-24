39be297dea140f82b0adcbcd673cc4caada63703
JoshuaPondrom <japrkf@mst.edu>
2020-02-24 15:16:09 -0600

    Purged react-bootstrap and cleansed with material-ui

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
-import { HomeNav } from './navs';
+import { Tab, Tabs } from '@material-ui/core';
+import { TabPanel } from './navs'
 
-export const App = () => (
-  <>
-    <HomeNav />
-    <div>
-      <p>Some random stufffff</p>
-    </div>
-  </>
-);
+export const App = () => {
+  const [val, setVal] = React.useState(0);
+
+  const handleChange = (event: any, newVal: any) => {
+    setVal(newVal);
+  };
+
+  return (
+    <>
+      <Tabs
+        value={val}
+        onChange={handleChange}
+      >
+        <Tab label="GitGrader" />
+        <Tab label="Canvas" />
+        <Tab label="Gitlab" />
+      </Tabs>
+      <TabPanel value={val} index={0}><p>The GitGrader tab</p></TabPanel>
+      <TabPanel value={val} index={1}><p>The Canvas tab</p></TabPanel>
+      <TabPanel value={val} index={2}><p>The Gitlab tab</p></TabPanel>
+    </>
+  );
+};
