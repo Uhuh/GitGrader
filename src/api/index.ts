2a8dd7d41182f4911b2a559c0b0aec48ffee166e
Dylan Warren <duwtgb@mst.edu>
2020-02-25 03:38:00 -0600

    Gitlab and canvas apis. Some basic methods for OUR api so we can create student repos and lock / unlock repos

--- src/api/index.ts
+++ src/api/index.ts
@@ -48,39 +48,95 @@ export class API {
 
   constructor (private info: ITokens) {
     this.gitlab = new GitlabBackend({ ...info });
-    this.canvas = new CanvasBackend();
+    this.canvas = new CanvasBackend({ ...info });
   }
   canvas_api = {
-    getClasses: (/* User token? */) => {},
-    getStudents: (class_id: string) => {}
+    getClasses: () => {
+      return this.canvas
+        .request('GET', 'courses', {
+          enrollment_type: 'ta',
+          enrollment_state: 'active'
+        })
+        .then(res => res)
+        .catch(console.error);
+    },
+    getStudents: (course_id: string) => {
+      return this.canvas
+        .students('GET', course_id)
+        .then(console.log)
+        .catch(console.error);
+    }
   };
   git_api = {
-    createAssignment: (assignment: string) => {
-      // Below is for testing purposes...
-      this.gitlab
-        .request('/namespaces', 'GET', { search: this.info.namespace })
-        .then(res => {
-          console.log(res);
-          if (!res) { 
-            return console.log('No namespace found?'); 
-          }
-        })
-        .catch();
+    // To help enforce formatting.
+    build_name: (sem: string, sec: string, n: string, user: string) => (
+      `${sem}-${sec}-${n}-${user}`
+    ),
+    // Creates BUT DOES NOT assign the repo for a user.
+    createAssignment: (
+      name: string,
+      namespace_id: string,
+      section: string,
+      semester: string,
+      username: string
+    ) => {
+      const params = {
+        'name': this.git_api.build_name(semester, section, name, username),
+        'namespace_id': namespace_id,
+        'issues_enabled': false,
+        'merge_requests_enabled': false,
+        'builds_enabled': false,
+        'wiki_enabled': false,
+        'snippets_enabled': true,
+        'visibility_level': GitVisibility.private,
+      };
+      // POST request to gitlab's projects to create a user repo in the selected group. (namespace)
+      return this.gitlab
+        .request('POST', '/projects', params)
+        .then(res => res)
+        .catch(console.error);
     },
     assignAssignment: (assignment: string, user_data: IRepoInfo) => {},
     archiveAssignment: (assignment: string) => {},
-    lockAssignment: (assignment: string) => {}
+    lockAssignment: (assignment_id: string, user_id: string) => {
+      const params = {
+        'id': assignment_id,
+        'user_id': user_id,
+        'access_level': GitAccess.reporter
+      };
+
+      return this.gitlab.edit_member('POST', params)
+    }
   };
 }
 
 class GitlabBackend {
   constructor (private info: IGitlab) {}
   request = async (
-    path: string,
     method: AxiosRequestConfig['method'],
+    path: string,
     params: {}
-  ) => {
+  ): Promise<any> => {
     const url = `${this.info.gitlab_host}/api/v4/${path}`;
+    return (
+      await axios({
+        method,
+        url,
+        headers: { 'Private-Token': this.info.gitlab_token },
+        params
+      })
+    ).data;
+  }
+  /* 
+    This allows us to change user level permissions
+    * Such as "lock" / "unlock" repo's for students
+  */
+  edit_member = async (
+    method: AxiosRequestConfig['method'],
+    params: { [k: string]: string }
+  ): Promise<any> => {
+    // id is the assignment id.
+    const url = `${this.info.gitlab_host}/api/v4/projects/${params.id}/members/${params.user_id}`;
     return (
       await axios({
         method,
@@ -93,5 +149,37 @@ class GitlabBackend {
 }
 
 class CanvasBackend {
+  constructor (private info: ICanvas) {}
+  request = async (
+    method: AxiosRequestConfig['method'],
+    path: string,
+    params: { [k: string]: string }
+  ): Promise<any> => {
+    const url = `${this.info.canvas_url}/api/v1/${path}`;
+    params.access_token = this.info.canvas_token;
+    return (
+      await axios({
+        method,
+        url,
+        params
+      })
+    ).data;
+  }
+  students = async (
+    method: AxiosRequestConfig['method'],
+    course_id: string
+  ): Promise<any> => {
+    const url = `${this.info.canvas_url}/api/v1/courses/${course_id}/enrollments`;
 
+    if(method !== 'GET') { return Promise.reject('Didn\'t use GET metod'); }
+    
+    // Currently it sends 10 per request, so limit is 1000 for now.
+    return (
+      await axios({
+        method,
+        url,
+        params: { access_token: this.info.canvas_token, per_page: 1000 }
+      })
+    ).data;
+  }
 }
