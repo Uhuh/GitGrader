53850c02f7a7b02631d5e256b867be75e0e3dd99
Alan Truong <atwpb@mst.edu>
2020-03-01 22:04:01 -0600

    able to show a list of all the classes onto the canvas page

--- src/modules/canvas/index.tsx
+++ src/modules/canvas/index.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import { CanvasBackend as Canvas, GitlabBackend as GL } from '../../api';
+import { FormControlStatic } from 'react-bootstrap';
 
-var data: string[];
-data = [];
+
+var data = [''];
 
 const CanvasAPI = new Canvas({
     canvas_url: 'https://mst.instructure.com',
-    canvas_token: '2006~rBsdDmvmuKgD629IaBL9zKZ3Xe1ggXHhcFWJH4eEiAgE62LUWemgbVrabrx116Rq'
+    canvas_token: ''
   });
 
 CanvasAPI.getClasses().then(function(result) {
-
+    data.pop()
     for(let i=0; i<result.length; i++){
         data.push(result[i].name);
     }
