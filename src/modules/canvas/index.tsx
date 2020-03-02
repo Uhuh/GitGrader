da44ef09bd8efe4e179ab1efdcd68352ea16c4c9
Alan Truong <atwpb@mst.edu>
2020-03-01 22:04:01 -0600

    able to show a list of all the classes onto the canvas page

--- src/modules/canvas/index.tsx
+++ src/modules/canvas/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Button, Dialog , DialogActions, DialogContent, 
   DialogContentText, DialogTitle ,Grid , makeStyles, TextField, Typography } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
@@ -179,38 +178,4 @@ export const CanvasPage = (props: { course: ICanvasNamespace; }) => {
           </Button>
       </Dialog>
     </>
-  );
-
-=======
-import * as React from 'react';
-import { CanvasBackend as Canvas, GitlabBackend as GL } from '../../api';
-
-var data: string[];
-data = [];
-
-const CanvasAPI = new Canvas({
-    canvas_url: 'https://mst.instructure.com',
-    canvas_token: '2006~rBsdDmvmuKgD629IaBL9zKZ3Xe1ggXHhcFWJH4eEiAgE62LUWemgbVrabrx116Rq'
-  });
-
-CanvasAPI.getClasses().then(function(result) {
-
-    for(let i=0; i<result.length; i++){
-        data.push(result[i].name);
-    }
-    
-});
-
-
-export const CanvasPage = () => {
-
-    return (
-        <ol>
-           {data.map(data => 
-            <li key={data.toString()}>   
-                {data} 
-            </li>)} 
-       </ol>
-      );
->>>>>>> able to show a list of all the classes onto the canvas page
-};
+  );};
