c78c0484a2af0900d38e9b0876375a42c51afdec
JoshuaPondrom <japrkf@mst.edu>
2020-03-02 14:56:37 -0600

    Card list component added. React-router should work now

--- src/modules/navs/index.tsx
+++ src/modules/navs/index.tsx
@@ -1,7 +1,6 @@
+import { AddCard } from './addCard';
 import { CourseCard } from './courseCard';
 import { CourseList } from './courseList';
 import { BackButton, SettingsButton, ThemeButton } from './floatingButtons';
-import { AddCard } from './addCard';
-
 
 export { CourseList, SettingsButton, BackButton, ThemeButton, AddCard };
