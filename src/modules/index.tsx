83cb98eaba583e096f9bd1ce97b6d3e424979231
Edward Tsang <eytrkf@mst.edu>
2020-03-04 01:58:27 -0600

    Hook issues

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -4,9 +4,6 @@ import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import { CanvasBackend as Canvas, GitlabBackend as GL } from '../api';
 import { CourseList } from './navs/courseList';
 
-/**
- * Make sure to use your token for testing. Might want to use an .env file for this
- */
 const GitLabAPI = new GL({
   gitlab_host: 'https://git-classes.mst.edu',
   gitlab_token: '',
