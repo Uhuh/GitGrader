d9f06addf4090f1bd10125257e733dac809e8b8e
Edward Tsang <eytrkf@umsystem.edu>
2020-03-04 04:01:16 -0600

    Made some page changes, trying to figure out input

--- src/modules/navs/index.tsx
+++ src/modules/navs/index.tsx
@@ -1,4 +1,19 @@
 import { CourseCard } from './courseCard';
 import { CourseList } from './courseList';
+import { Button } from '@material-ui/core';
+import * as React from 'react';
+
+export const Navigation = () => {
+    return (
+      <div>
+        <Button href="#/courses">
+          Courses
+        </Button>
+        <Button href="#/setup">
+          Settings
+        </Button>
+      </div>
+    )
+  }
 
 export { CourseList };
