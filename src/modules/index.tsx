227730b45bdc15fb31d17e6c2c71f840ace6f690
Edward Tsang <eytrkf@mst.edu>
2020-03-04 01:58:27 -0600

    Hook issues

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,11 +1,9 @@
-import { Tab, Tabs } from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent,
+         DialogContentText, DialogTitle, Tab, Tabs, TextField } from '@material-ui/core';
 import * as React from 'react';
 import { CanvasBackend as Canvas, GitlabBackend as GL } from '../api';
 import { TabPanel } from './navs';
 
-/**
- * Make sure to use your token for testing. Might want to use an .env file for this
- */
 const GitLabAPI = new GL({
   gitlab_host: 'https://git-classes.mst.edu',
   gitlab_token: '',
@@ -17,24 +15,39 @@ const CanvasAPI = new Canvas({
   canvas_token: ''
 });
 
-/* GitLabAPI.createAssignment(
-  'hw1',
-  '2453',
-  '001',
-  '2020-SP',
-  'duwtgb'
-)
-.then(console.log)
-.catch(console.error); */
+export const firstTimeDialog = () => {
+  const [open, setOpen] = React.useState(false);
 
-/* GitLabAPI.lockAssignment('', '')
-  .then(console.log)
-  .catch(console.error); */
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-/* canvas.getStudents('')
-  .then(console.log)
-  .catch(console.error);
- */
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>First Time Setup</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <TextField id="standard-basic" label="GitLab Access Key" />
+            <TextField id="standard-basic" label="Canvas Access Key" />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>
+            Done
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 
 export const App = () => {
   const [val, setVal] = React.useState(0);
@@ -53,7 +66,10 @@ export const App = () => {
         <Tab label='Canvas' />
         <Tab label='Gitlab' />
       </Tabs>
-      <TabPanel value={val} index={0}><p>The GitGrader tab</p></TabPanel>
+      <TabPanel value={val} index={0}>
+        <p>The GitGrader tab</p>
+        <Button variant="outlined" color="primary" onClick={firstTimeDialog}>Setup</Button>
+      </TabPanel>
       <TabPanel value={val} index={1}><p>The Canvas tab</p></TabPanel>
       <TabPanel value={val} index={2}><p>The Gitlab tab</p></TabPanel>
     </>
