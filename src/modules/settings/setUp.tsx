5a0d1b5d4d3d9286b5eb9c67cc68ca274cf3e1fd
Edward Tsang <eytrkf@umsystem.edu>
2020-03-09 12:34:08 -0500

    Accepts inputs; need to get input into file

--- src/modules/settings/setUp.tsx
+++ src/modules/settings/setUp.tsx
@@ -1,40 +1,74 @@
-import { Button, Input, TextField } from '@material-ui/core';
+import { Button, FormControl, Grid, InputLabel, OutlinedInput } from '@material-ui/core';
 import * as React from 'react';
 import styled from 'styled-components';
 
-const CenteredDiv = styled.div`
-  width: 500px;
-  margin-left: auto;
-  margin-right: auto;
+const SpacePadding = styled.div`
+  margin-bottom: 20px;
 `;
 
-export const SetUp = () => {  
+export const SetUp = () => {
+  const [canvasHost, setCanvasHost] = React.useState(' ');
+  const [gitlabHost, setGitlabHost] = React.useState(' ');
+  const [canvasKey, setCanvasKey] = React.useState(' ');
+  const [gitlabKey, setGitlabKey] = React.useState(' ');
+
+  const handleChangeCH = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCanvasHost(event.target.value);
+  };
+
+  const handleChangeGH = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGitlabHost(event.target.value);
+  };
+
+  const handleChangeCK = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCanvasKey(event.target.value);
+  };
+
+  const handleChangeGK = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGitlabKey(event.target.value);
+  };
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Canvas Host:', canvasHost, 'GitLab Host:', gitlabHost);
+    console.log('Canvas Key:', canvasKey, 'GitLab Key:', gitlabKey);
+  };
+
   return ( 
-    <CenteredDiv>
-      <form>
-        <div>
-          <TextField
-              id='canvasHost'
-              label='Canvas Host'
-              variant='outlined'
-              />
-            <TextField
-              id='gitlabHost'
-              label='GitLab Host'
-              variant='outlined'
-              />
-        </div>
-        <TextField
-          id='canvasKey'
-          label='Canvas Access Key'
-          variant='outlined'
-          />
-        <TextField
-          id='gitlabKey'
-          label='GitLab Access Key'
-          variant='outlined'
-          />
+    <Grid 
+     container
+     direction='column'
+     alignItems='center' 
+     justify='center'
+    >
+      <form onSubmit={handleSubmit} noValidate autoComplete='off'>
+        <FormControl variant='outlined'>
+          <InputLabel htmlFor='canvasHost'>Canvas Host</InputLabel>
+          <OutlinedInput id='canvasHost' value={canvasHost} onChange={handleChangeCH} label='canvasHost' />
+        </FormControl>
+        <FormControl variant='outlined'>
+          <InputLabel htmlFor='gitlabHost'>GitLab Host</InputLabel>
+          <OutlinedInput id='gitlabHost' value={gitlabHost} onChange={handleChangeGH} label='gitlabHost' />
+        </FormControl>
+        <SpacePadding></SpacePadding>
+        <FormControl variant='outlined'>
+          <InputLabel htmlFor='canvasKey'>Canvas Key</InputLabel>
+          <OutlinedInput id='canvasKey' value={canvasKey} onChange={handleChangeCK} label='canvasKey' />
+        </FormControl>
+        <FormControl variant='outlined'>
+          <InputLabel htmlFor='gitlabKey'>GitLab Key</InputLabel>
+          <OutlinedInput id='gitlabKey' value={gitlabKey} onChange={handleChangeGK} label='gitlabKey' />
+        </FormControl>
+        <SpacePadding></SpacePadding>
+        <Grid 
+         container
+         direction='column'
+         alignItems='center' 
+         justify='center'
+        >   
+          <Button variant='outlined' type='submit'>Save</Button>
+        </Grid>
       </form>
-    </CenteredDiv>
+    </Grid>
   );
 };
