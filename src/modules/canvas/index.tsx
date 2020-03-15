3d28dfd5bc56a24ee6337d4d9e1184ed3be79249
Alan Truong <atwpb@mst.edu>
2020-03-14 20:57:45 -0500

    added some card styling

--- src/modules/canvas/index.tsx
+++ src/modules/canvas/index.tsx
@@ -1,12 +1,38 @@
 import { Box } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent , CardHeader, makeStyles, Typography} from '@material-ui/core/';
+import  AddIcon from '@material-ui/icons/Add';
 import * as React from 'react';
 import { CanvasBackend as Canvas, GitlabBackend as GL } from '../../api';
 import { ICanvasClass , ICanvasUser, IGitNamespace } from '../../api/interfaces';
-import { CourseList } from '../navs';
 
 const CanvasAPI = new Canvas({
   canvas_url: 'https://mst.instructure.com',
   canvas_token: '2006~rBsdDmvmuKgD629IaBL9zKZ3Xe1ggXHhcFWJH4eEiAgE62LUWemgbVrabrx116Rq'
+});
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 275,
+    maxHeight: 200,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  addIcon: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 50,
+    height: 50,
+  },
+  centerItem: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  }
 });
 
 /**
@@ -16,6 +42,7 @@ const CanvasAPI = new Canvas({
  */
 export const CanvasPage = (props: { courseId: string; courses: ICanvasClass[]; namespace: IGitNamespace[]; }) => {
   const { courseId, courses, namespace } = props;
+  const classes = useStyles();
   
   let classIndex = 0;
   const [assignmentName, setAssignmentName] = React.useState('');
@@ -49,20 +76,35 @@ export const CanvasPage = (props: { courseId: string; courses: ICanvasClass[]; n
           Course Instructor(s): 
           {courses[classIndex].teachers.map(teacher => <li>{teacher.display_name}</li>)}
         </p>
-        <div>
-          <label>Namespace</label>
-          <select className='namespace'>
+        <br />
+        <div className='namespace'>
             {namespace.map(namespace => 
-              <option key={namespace.id} value={namespace.name}>{namespace.name}</option>
+            <div>
+              <Card className={classes.root}>
+                <CardContent>
+                    <Typography className={classes.title} color='textSecondary' gutterBottom>
+                      {namespace.name}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size='small'>Learn More</Button>
+                </CardActions>
+              </Card>
+              <br />
+            </div>
             )}
-          </select>
         </div>
         <div>
-            <label>
-              Assignment Name:
-              <input type='text' onChange={e => setAssignmentName(e.target.value)}/>
-            </label>      
-          <button onClick={handleSubmit}>hello</button>
+          <Card className={classes.root}>
+            <br />
+            <AddIcon className={classes.addIcon}></AddIcon>
+            <br />
+          </Card>
+              <label>
+                Assignment Name:
+                <input type='text' onChange={e => setAssignmentName(e.target.value)}/>
+              </label>      
+            <button onClick={handleSubmit}>hello</button>
         </div>
       </div>
     </Box>
