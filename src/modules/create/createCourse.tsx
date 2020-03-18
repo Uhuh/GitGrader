22026df126c8a98ec347685657c210c223729135
Joshua Pondrom <japrkf@mst.edu>
2020-03-18 05:09:01 -0500

    real :monkey: coding going on here

--- src/modules/create/createCourse.tsx
+++ src/modules/create/createCourse.tsx
@@ -1,100 +1,121 @@
 import { Button, createStyles, FormControl, Grid, InputLabel, makeStyles, Paper, Select, Theme } from '@material-ui/core';
 import LinkIcon from '@material-ui/icons/Link';
 import * as React from 'react';
-import { CanvasBackend as Canvas, GitlabBackend as GL } from '../../api';
-import { ICanvasClass } from '../../api/interfaces';
+import { ICanvasClass, IGitNamespace } from '../../api/interfaces';
+import {CanvasAPI, GitLabAPI} from '../../modules';
 
 export const CreateCourse = (canvasIDs: any[], gitlabIDs: any[]) => {
   const [courses, setCourses] = React.useState<ICanvasClass[]>();
-  // const [namespaces, setNamespaces] = React.useState<IGitNamespace[]>();
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    console.log(event.target);
-    // const temp = JSON.parse(localStorage.getItem('courses') || '{}');
-    // temp[`${event.target.canvasID}`] = event.target.gitlabID;
-    // localStorage.setItem('courses', JSON.stringify(temp));
+  const [namespaces, setNamespaces] = React.useState<IGitNamespace[]>();
+
+  const [selectedNamespace, setSelectedNamespace] = React.useState('');
+  const [selectedCourse, setSelectedCourse] = React.useState('');
+
+  const handleChangeNamespace = (event: React.ChangeEvent<{ value: unknown }>) => {
+    if(typeof event.target.value === 'string'){
+      setSelectedNamespace(event.target.value);
+    }
+  }; 
+  
+  const handleChangeCourse = (event: React.ChangeEvent<{ value: unknown }>) => {
+    if (typeof event.target.value === 'string') {
+      setSelectedCourse(event.target.value);
+    }
   };
 
-  const CanvasAPI = new Canvas({
-    canvas_token: '2006~rBsdDmvmuKgD629IaBL9zKZ3Xe1ggXHhcFWJH4eEiAgE62LUWemgbVrabrx116Rq',
-    canvas_url: 'https://mst.instructure.com',
-  });
-  const GitLabAPI = new GL({
-    gitlab_token: 'hwFLotsH2w61iEu3EC-f',
-    gitlab_host: 'https://gitlab.instructure.com',
-    namespace: '2020-senior-test',
-  }); 
+  const handleSubmit = () => {
+    const temp = JSON.parse(localStorage.getItem('relations') || '{}');
+    temp[selectedCourse] = {canvasID: selectedCourse, gitlabID: selectedNamespace};
+    localStorage.setItem('relations', JSON.stringify(temp));
+  }
   
   React.useEffect(() => {
     CanvasAPI.getClasses()
-      .then(classes => {
-        setCourses(classes);
+      .then(courses => {
+        setCourses(courses);
       })
       .catch(console.error);
   }, [CanvasAPI]);
 
-  // React.useEffect(() => {
-  //   GitLabAPI.getNamespaces()
-  //     .then(namespaces => {
-  //       setNamespaces(namespaces);
-  //     })
-  //     .catch(console.error);
-  // }, [GitlabAPI]);
+  React.useEffect(() => {
+    GitLabAPI.getNamespaces()
+      .then(namespaces => {
+        setNamespaces(namespaces);
+      })
+      .catch(console.error);
+  }, [GitLabAPI]);
 
   return (
     <Grid
       container
       spacing={0}
-      direction="row"
-      alignItems="center"
-      justify="center"
+      direction='column'
+      alignItems='center'
+      justify='center'
       style={{ minHeight: '100vh' }}
     >
       <Grid item xs={3} style={{ minHeight: '70vh', minWidth: '60vh' }}>
         <Paper elevation={3} style={{ minHeight: '70vh', minWidth: '60vh', padding: '20px'}}>
-          <FormControl>
-            <InputLabel id='canvasIDlabel'>Canvas ID</InputLabel>
-            <Select
-              native
-              onChange={handleChange}
-              inputProps={{
-                id: 'canvasID'
-              }}
-            >
-              {
-                courses ? courses.map(course => (
-                  <option key={course.id} value={course.name}>
-                    {course.name}
-                  </option>
-                )) : 
-                <option key='null' value='null'>
-                  "No courses found"
-                </option>
-              })}
-            </Select>
-            <LinkIcon />
-            <InputLabel id='githubIDlabel'>Gitlab ID</InputLabel>
-            <Select
-              native
-              onChange={handleChange}
-              inputProps={{
-                id: 'gitlabID'
-              }}
-            >
-              {
-                courses ? courses.map(course => (
-                  <option key={course.id} value={course.name}>
-                    {course.name}
-                  </option>
-                )) :
+          <Grid item>
+            <FormControl>
+              <InputLabel id='canvasIDlabel'>Canvas ID</InputLabel>
+              <Select
+                native
+                onChange={handleChangeCourse}
+                label='canvasIDlabel'
+                value={selectedCourse}
+                inputProps={{
+                  id: 'canvasID'
+                }}
+                >
+                {
+                  courses ? courses.map(course => (
+                    <option key={course.id} value={course.id}>
+                      {course.name}
+                    </option>
+                  )) : 
                   <option key='null' value='null'>
                     "No courses found"
-                </option>
-              })}
-            </Select>
-            <Button>
-              Create
-            </Button>
-          </FormControl>
+                  </option>
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <LinkIcon />
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <InputLabel id='githubIDlabel'>Gitlab ID</InputLabel>
+              <Select
+                native
+                onChange={handleChangeNamespace}
+                label='githubIDlabel'
+                value={selectedNamespace}
+                inputProps={{
+                  id: 'gitlabID'
+                }}
+                >
+                {
+                  namespaces ? namespaces.map(namespace => (
+                    <option key={namespace.id} value={namespace.id}>
+                      {namespace.name}
+                    </option>
+                  )) :
+                  <option key='null' value='null'>
+                      "No namespaces found"
+                  </option>
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <Button onClick={handleSubmit} type='submit'>
+                Create
+              </Button>
+            </FormControl>
+          </Grid>
         </Paper>
       </Grid>
     </Grid>
