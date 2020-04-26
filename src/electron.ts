1b53d1f3ebea47cafccce89363568999a3175463
Joshua Pondrom <japrkf@mst.edu>
2020-04-26 02:14:09 -0500

    shit bro it bundles

--- src/electron.ts
+++ src/electron.ts
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require("electron-is-dev");
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -11,6 +13,8 @@ const createWindow = () => {
 
   // Load the index.html so we can ... insert html.
   win.loadFile('index.html');
+  // fix this shit so it bundles right
+  // win.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../dist/index.html")}`);
 
   win.webContents.openDevTools();
 };
