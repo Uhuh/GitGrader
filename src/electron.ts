2685318b38754bf8e2d37b3f39f0e3202f88d242
Joshua Pondrom <japrkf@mst.edu>
2020-04-28 16:49:09 -0500

    more work towards packaging

--- src/electron.ts
+++ src/electron.ts
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const url = require('url');
 const isDev = require("electron-is-dev");
 
 const createWindow = () => {
@@ -12,11 +13,13 @@ const createWindow = () => {
   });
 
   // Load the index.html so we can ... insert html.
+  console.log(__dirname);
+  // win.loadURL(url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: 'file',
+  //   slashes: true
+  // }));
   win.loadFile('index.html');
-  // fix this shit so it bundles right
-  // win.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../dist/index.html")}`);
-
-  win.webContents.openDevTools();
 };
 
 app.whenReady()
